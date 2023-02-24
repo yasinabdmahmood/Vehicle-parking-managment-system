1a59c7e479c8d7cc895c961383e79b488155ded0
yasinabdmahmood <abd181103@gmail.com>
2023-02-24 20:04:55 +0300

    Add basic sidebar to the project

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import styles from './home.module.scss'
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar'
 function Home() {
   return (
+    <>
     <div className={styles["scssClass"]}>
       <h2>React Js SASS / SCSS Example</h2>
     </div>
+    
+
+    <Sidebar>
+      <Menu>
+        <SubMenu label="Charts">
+          <MenuItem> Pie charts </MenuItem>
+          <MenuItem> Line charts </MenuItem>
+        </SubMenu>
+        <MenuItem> Documentation </MenuItem>
+        <MenuItem> Calendar </MenuItem>
+      </Menu>
+    </Sidebar>
+    </>
+
   )
 }
 export default Home
