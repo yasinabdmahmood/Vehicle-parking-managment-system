edbabc7ab6ca23511419eb213aea94e5309d9000
yasinabdmahmood <abd181103@gmail.com>
2023-02-24 11:06:21 +0300

    Modify the styling structure

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import styles from './home.module.scss'
 function Home() {
   return (
-    <div className="scssClass">
+    <div className={styles["scssClass"]}>
       <h2>React Js SASS / SCSS Example</h2>
     </div>
   )
