33a28030d02c944d8280b6db016f1355d6af9538
yasinabdmahmood <abd181103@gmail.com>
2023-02-26 14:11:00 +0300

    Set up routes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,22 @@
-import React from 'react'
-import Template from '../template/Template'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Template from '../template/Template';
+import Login from '../login/Login';
+import Signup from '../signup/Signup';
+import Dashboard from '../dashboard/Dashboard';
+import AddVehicle from '../add_vehicle/AddVehicle';
 function Home() {
   return (
     <>
-      <Template />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        {/* <Route path="/home" element={<Template />} /> */}
+          <Route path="/home" element={<Template />}>
+            <Route index element={<Dashboard/>} />
+            <Route path="add_vehicle" element={<AddVehicle/>} />
+          </Route>
+        </Routes>
     </>
 
   )
