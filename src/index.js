9b1636a464cb8e9cfe3f373ecf84bfd0c5404c1e
yasinabdmahmood <abd181103@gmail.com>
2023-02-26 14:10:08 +0300

    Add BrowserRouter to wrap the entire component tree

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ProSidebarProvider } from 'react-pro-sidebar';
+import { BrowserRouter} from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter>
   <React.StrictMode>
     <ProSidebarProvider>
       <App />
     </ProSidebarProvider>
   </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
